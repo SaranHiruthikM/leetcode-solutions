e75384ea980d0878b260aa848726d76b2b51364c
Saran Hiruthik M <saran.hiruthik83@gmail.com>
2026-09-08 02:31:25 +0530

    Time: 1 ms (99.99%) | Memory: 45.3 MB (86.83%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+
+    private void dfs(TreeNode root, List<Integer> temp, int targetSum){
+        if(root == null){
+            return;
+        }
+
+        temp.add(root.val);
+        if(root.left == null && root.right == null && targetSum == root.val){
+            res.add(new ArrayList<>(temp));
+        }
+
+        dfs(root.left, temp, targetSum-root.val);
+        dfs(root.right, temp, targetSum-root.val);
+
+        temp.remove(temp.size()-1);
+    }
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        dfs(root, new ArrayList<>(), targetSum);
+        return res;
+    }
+}
